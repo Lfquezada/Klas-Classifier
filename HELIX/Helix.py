a628298a54b3818d8f8a17397169ec61fa4d66da
lquezada <que18028@uvg.edu.gt>
2020-02-17 00:49:07 -0600

    Helix predict added

--- HELIX/Helix.py
+++ HELIX/Helix.py
@@ -9,8 +9,8 @@ print('\n\t\tH  E  L  I  X\n')
 
 # Libraries
 import pandas as pd
+import numpy as np
 from scipy import stats
-from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix,accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -22,7 +22,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
 # Train all models and return the one with best accuracy
-def getBestFitModel(X,y,scaled,testSize):
+def getBestFitModel(X,y,scaled,testSize,returnAllResults):
 	# Vars
 	allAlgorithmsNames = []
 	allKs = [3,5,7,11,13,15]
@@ -77,35 +77,68 @@ def getBestFitModel(X,y,scaled,testSize):
 	bestFitAlgorithmName = allAlgorithmsNames[bestAccuracyIndex]
 	bestFitClassifier = classifiers[bestAccuracyIndex]
 
-	return bestFitClassifier,bestFitAlgorithmName,bestAccuracy
+	if returnAllResults:
+		return bestFitClassifier,bestFitAlgorithmName,bestAccuracy,allAlgorithmsNames,allResults
+	else:
+		return bestFitClassifier,bestFitAlgorithmName,bestAccuracy
 
 
-def showResults():
-	# Showing results for all algorithms
-	for i in range(0,len(allAlgorithmsNames)):
-		print(str(allAlgorithmsNames[i]) + ': ' + str(allResults[i]))
+def predict(X,y,scaled,testSize,threshold,inputX):
+
+	# Vars
+	allKs = [3,5,7,11,13,15]
+	allKernelModes = ['linear','rbf','poly','sigmoid']
+	classifiers = []
+	allResults = []
+
+	# Splitting into training set and test set
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = testSize, random_state = 0)
+
+	# Feature Scaling
+	if scaled:
+		sc = StandardScaler()
+		X_train = sc.fit_transform(X_train)
+		X_test = sc.transform(X_test)
+		inputX = sc.transform(inputX)
 
 
+	# Decision Tree Classification
+	classifiers.append(DecisionTreeClassifier(criterion = 'entropy', random_state = 0))
 
-'''
-print('\n>< Predict')
-inputData = [[5.5,2.6,4.4,1.2]]
-#inputData = sc.transform(inputData)
-	
-allPredictions = []
+	# Random Forest Classification
+	classifiers.append(RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0))
 
-for classifier in classifiers:
-	allPredictions.append(classifier.predict(inputData))
+	# SVM
+	for kernelMode in allKernelModes:
+		classifiers.append(SVC(kernel = kernelMode, random_state = 0))
 
-finalPred = int(stats.mode(allPredictions)[0][0])
+	# K-NN
+	for k in allKs:
+		classifiers.append(KNeighborsClassifier(n_neighbors = k, metric = 'minkowski', p = 2))
 
-if finalPred == 1:
-	print('Iris-setosa')
-elif finalPred == 2:
-	print('Iris-versicolor')
-elif finalPred == 3:
-	print('Iris-virginica')
-'''
+	# Naive Bayes
+	classifiers.append(GaussianNB())
+
+	# Logistic Regression
+	classifiers.append(LogisticRegression(random_state = 0))
+
+	# Fitting to all
+	for classifier in classifiers:
+		classifier.fit(X_train, y_train)
+		y_pred = classifier.predict(X_test)
+		allResults.append(accuracy_score(y_test,y_pred))
+
+	allPredictions = []
+
+	# only use for predicting those models whose accuracy is >= the threshold set
+	for i in range(0,len(classifiers)):
+		if allResults[i] >= threshold:
+			allPredictions.append(classifiers[i].predict(inputX))
+
+	# the most common prediction from all models is selected as the final one
+	finalPred = int(stats.mode(allPredictions)[0][0])
+
+	return finalPred
 
 
 
--- HELIX/testHelix.py
+++ HELIX/testHelix.py
@@ -2,7 +2,8 @@
 
 # testing Helix Module
 import pandas as pd
-from Helix import getBestFitModel
+import matplotlib.pyplot as plt
+from Helix import getBestFitModel,predict
 
 # Import dataset
 dataset = pd.read_csv('iris-data-clean.csv')
@@ -15,16 +16,31 @@ dataset['class'] = dataset['class'].astype('float64')
 X = dataset.iloc[:, [0,1,2,3]].values
 y = dataset.iloc[:, 4].values
 
-classifier,name,accuracy = getBestFitModel(X,y,scaled=False,testSize=0.25)
+classifier,name,accuracy,allNames,allResults = getBestFitModel(X,y,scaled=False,testSize=0.25,returnAllResults=True)
 
 print('\n>< Best fit algorithm: ' + name)
 print('>< Accuracy: ' + str(accuracy*100)[:4] + '%')
 
-print('\n>< Predict')
-inputData = [[5.5,2.6,4.4,1.2]]
-#inputData = sc.transform(inputData)
+# Showing results for all algorithms
+plt.barh(allNames, allResults, align='center')
+plt.xlabel('Accuracy')
+plt.title('All results')
+plt.show()
 
+# Predictions
+print('\n>< Prediction')
+
+inputData = [[5.5,2.6,4.4,1.2]]
+
+# Predict via the best fit model
+'''
+#inputData = sc.transform(inputData)
 finalPred = int(classifier.predict(inputData))
+'''
+
+# Predict via Helix.predict
+finalPred = predict(X,y,scaled=False,testSize=0.25,threshold=0.8,inputX=inputData)
+
 
 if finalPred == 1:
 	print('Iris-setosa')
